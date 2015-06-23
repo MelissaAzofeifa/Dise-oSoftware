df20d1a3424c5518df816d41227cb1b8732384fb
MelissaAzofeifa <meli_azo@hotmail.com>
2015-06-22 19:21:25 -0600

    Envio sem7

---
+++ Division/Division/Division/CalculadoraBoundary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division
+{
+    class CalculadoraBoundary
+    {
+        double ObtenerDivisor()
+        {
+            string divisor = Console.ReadLine();
+            double respuesta = double.Parse(divisor);
+            return respuesta;
+        }
+    }
+}
